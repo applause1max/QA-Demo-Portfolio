b699c0670a1d9a9282fa375b3e8fe5a2cbd860b3
Maksim Ometcinskii <applause1max@gmail.com>
2026-07-05 22:24:15 -0400

    Add TC-003 through TC-006 login edge case tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,10 +6,10 @@ PASSWORD = "secret_sauce"
 
 def test_login_valid():
 
-    """ TC-001: Valid login redirects to inventory page. """
+    """TC-001: Valid login redirects to inventory page."""
     
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto("https://www.saucedemo.com/")
@@ -28,7 +28,7 @@ def test_login_locked_out():
     """TC-002: Locked Out login shows corresponding error and stays within same URL."""
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto("https://www.saucedemo.com/")
@@ -40,3 +40,78 @@ def test_login_locked_out():
         expect(page.get_by_text("locked out.")).to_be_visible()
 
         browser.close()
+
+
+
+def test_login_wrong_password():
+
+    """TC-003: Invalid login with wrong password."""
+    
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.get_by_role("textbox", name="Username").fill(STANDARD_USER)
+        page.get_by_role("textbox", name="Password").fill("Wrong_password")
+        page.get_by_role("button", name="Login").click()
+
+        expect(page).to_have_url("https://www.saucedemo.com/")
+        expect(page.get_by_text("Username and password do not match any user in this service")).to_be_visible()
+
+        browser.close()
+
+
+def test_login_empty_username():
+
+    """TC-004: Invalid login with empty username."""
+    
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.get_by_role("textbox", name="Password").fill(PASSWORD)
+        page.get_by_role("button", name="Login").click()
+
+
+        expect(page).to_have_url("https://www.saucedemo.com/")
+        expect(page.get_by_text("Username is required")).to_be_visible()
+
+        browser.close()
+
+
+def test_login_empty_password():
+
+    """TC-005: Invalid login with empty password."""
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.get_by_role("textbox", name="Username").fill(STANDARD_USER)
+        page.get_by_role("button", name="Login").click()
+
+        expect(page).to_have_url("https://www.saucedemo.com/")
+        expect(page.get_by_text("Password is required")).to_be_visible()
+
+        browser.close()
+
+
+def test_login_empty_fields():
+
+    """TC-006: Invalid login with empty fields."""
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.get_by_role("button", name="Login").click()
+
+        expect(page).to_have_url("https://www.saucedemo.com/")
+        expect(page.get_by_text("Username is required")).to_be_visible()
+
+        browser.close()
+
