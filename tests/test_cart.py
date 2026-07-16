2876a478380f989300fc037b8af0f6efd662970d
Maksim Ometcinskii <applause1max@gmail.com>
2026-07-16 19:39:59 -0400

    Added happy path checkout test case

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -45,3 +45,33 @@ def test_remove_item_from_cart():
 
 
 
+def test_checkout_happy_path():
+
+    """TC-019: Complete checkout with valid information and items in cart."""
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://www.saucedemo.com/")
+        page.get_by_role("textbox", name="Username").fill(STANDARD_USER)
+        page.get_by_role("textbox", name="Password").fill(PASSWORD)
+        page.get_by_role("button", name="Login").click()
+
+        page.get_by_role("button", name="Add to cart").first.click()
+        page.locator("[data-test='shopping-cart-link']").click()
+        page.get_by_role("button", name="Checkout").click()
+        page.get_by_role("textbox", name="First Name").fill("Test")
+        page.get_by_role("textbox", name="Last Name").fill("User")
+        page.get_by_role("textbox", name="Zip/Postal Code").fill("90210")
+        page.get_by_role("button", name="Continue").click()
+        page.get_by_role("button", name="Finish").click()
+
+        expect(page.locator("[data-test='complete-header']")).to_have_text("Thank you for your order!")
+        expect(page).to_have_url("https://www.saucedemo.com/checkout-complete.html")
+
+        browser.close()
+
+
+
+    
